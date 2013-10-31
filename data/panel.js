c62a1c92b60f82224b028cd453c4b8b90ae3820c
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-10-31 16:26:04 -0700

    able to download a zip

--- data/panel.js
+++ data/panel.js
@@ -1,15 +1,14 @@
-document.body.style.border = "5px solid red";
-
-self.postMessage({ type: "test", data: "hello world" }, "*");
-self.port.on("hello", function (data) {
-  document.body.style.border = "5px solid green";
-});
+function fetch () {
+  self.postMessage({ type: "downloadAdb" });
+};
 
 self.port.on("initialPrefs", function (data) {
-  if (data.adbPath) {
-    document.getElementById("adb_install").style.border = "5px solid red";
+  if (!data.adbPath) {
+    let b = document.getElementById("adb_install");
+    b.style.border = "5px solid red";
+    b.addEventListener("click", fetch);
   }
-  if (data.fastbootPath) {
+  if (!data.fastbootPath) {
     document.getElementById("fastboot_install").style.border = "5px solid red";
   }
   console.log(JSON.stringify(data));
