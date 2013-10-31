fdc577bbce570037a5861519552397e0e045707c
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-10-31 15:35:49 -0700

    able to communicate back and forth

--- data/panel.js
+++ data/panel.js
@@ -1,3 +1,16 @@
 document.body.style.border = "5px solid red";
 
 self.postMessage({ type: "test", data: "hello world" }, "*");
+self.port.on("hello", function (data) {
+  document.body.style.border = "5px solid green";
+});
+
+self.port.on("initialPrefs", function (data) {
+  if (data.adbPath) {
+    document.getElementById("adb_install").style.border = "5px solid red";
+  }
+  if (data.fastbootPath) {
+    document.getElementById("fastboot_install").style.border = "5px solid red";
+  }
+  console.log(JSON.stringify(data));
+});
