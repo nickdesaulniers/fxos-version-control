97dcdebc13ec54727ceb3ddc17d7e285e6817d7b
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-11-06 16:03:59 -0800

    hook up progress bar

--- data/panel.js
+++ data/panel.js
@@ -6,22 +6,40 @@ function send (type, data) {
 };
 
 function fetch () {
+  let ele = document.getElementById("adb_progress");
+  ele.style.display = "inline";
   send("downloadAdb");
 };
 
-self.port.on("initialPrefs", function (data) {
-  if (!data.adbPath) {
-    let b = document.getElementById("adb_install");
+function onInitialPrefs(data) {
+  let b = document.getElementById("adb_install");
+  if (data.adbPath) {
+    document.getElementById("adb_path").textContent = data.adbPath;
+  } else {
     b.style.border = "5px solid red";
-    b.addEventListener("click", fetch);
   }
+  b.addEventListener("click", fetch);
   if (!data.fastbootPath) {
     document.getElementById("fastboot_install").style.border = "5px solid red";
   }
-});
+};
 
-self.port.on("download", function (data) {
+function onDownload (data) {
   if (DEBUG) console.log("panel.js received a download event: " + data);
-});
+  let ele = document.getElementById('adb_progress');
+  ele.value = data.value;
+  ele.max = data.max;
+};
+
+function onDownloadDone (data) {
+  let ele = document.getElementById("adb_progress");
+  ele.value = 0;
+  ele.style.display = "none";
+  document.getElementById("adb_path").textContent = data.adbPath;
+};
+
+self.port.on("initialPrefs", onInitialPrefs);
+self.port.on("download", onDownload);
+self.port.on("downloadDone", onDownloadDone);
 
 send("initialPrefs");
